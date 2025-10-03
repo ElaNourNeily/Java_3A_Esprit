5f9a6d837f568ede08bce845fdfc584ed76b6b33
ElaNourNeily <elaenour.neily@esprit.tn>
2025-10-03 20:11:25 +0100

    prosit_3

--- Main.java
+++ Main.java
@@ -14,14 +14,59 @@ class Animal  {
     }
 }
 class Zoo  {
-    Animal[] animals = new Animal[25];
+    public static final int MAX_CAGES = 25;
+    Animal[] animals ;
     String name;
     String city;
-    int nbeCages;
+    final int nbeCages;
+    int nbA = 0;
     Zoo (String name, String city, int nbeCages) {
         this.name = name;
         this.city = city;
-        this.nbeCages = nbeCages;
+        this.nbeCages = (nbeCages > MAX_CAGES) ? MAX_CAGES : nbeCages;
+        //if (nbeCages > MAX_CAGES) {
+        //    this.nbeCages = MAX_CAGES;
+        //} else {
+        //    this.nbeCages = nbeCages;
+        //}
+        this.animals = new Animal[nbeCages];
+    }
+    boolean isFull() {
+        return nbA >= nbeCages;
+    }
+    static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        if (z1.nbA >= z2.nbA) {
+            return z1;
+        } else {
+            return z2;
+        }
+    }
+    boolean addAnimal(Animal a) {
+        if (nbA >= animals.length) {
+            return false;
+        }
+        for (int i = 0; i < nbA; i++) {
+            if (animals[i].name.equals(a.name)) {
+                return false;
+            }
+        }
+        animals[nbA] = a;
+        nbA++;
+        return true;
+    }
+    void displayAnimals() {
+        System.out.println(" animaux" + name + ":");
+        for (int i = 0; i < nbA; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+    int searchAnimal(String name) {
+        for (int i = 0; i < nbA; i++) {
+            if (animals[i].name.equals(name)) {
+                return i;
+            }
+        }
+        return -1;
     }
     void displayZoo() {
         System.out.println("Zoo: " + name + " city : " + city );
@@ -38,16 +83,31 @@ public class Main {
         Animal dog = new Animal("dogs","roky",5,true);
 
 
-        Zoo myZoo = new Zoo("zoo1","tunis",20);
+        //Zoo myZoo = new Zoo("zoo1","tunis",50);
+        //System.out.println("Capacité max du zoo : " + myZoo.nbeCages);
+        Zoo myZoo = new Zoo("zoo1","tunis",2);
+        Zoo zoo1 = new Zoo("zoo1", "ariena", 5);
+        //myZoo.animals[0] = lion;
+        //myZoo.animals[1] = cat;
+        //myZoo.animals[2] = dog;
 
-        myZoo.animals[0] = lion;
-        myZoo.animals[1] = cat;
-        myZoo.animals[2] = dog;
-
-        myZoo.displayZoo();
+        //myZoo.displayZoo();
         //System.out.println(myZoo);
         //System.out.println(myZoo.toString());
-        System.out.println(lion);
-        System.out.println(myZoo);
+        //System.out.println(lion);
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(cat));
+        System.out.println(myZoo.addAnimal(dog));
+        //System.out.println(myZoo.addAnimal(lion));
+        System.out.println("myZoo plein ? " + myZoo.isFull());
+        System.out.println("zoo1 plein ? " + zoo1.isFull());
+        Zoo biggest = Zoo.comparerZoo(myZoo, zoo1);
+        System.out.println("Le zoo avec plus d’animaux est : " + biggest.name);
+        myZoo.displayAnimals();
+
+        //System.out.println("Recherche de 'moche' : " + myZoo.searchAnimal("moche"));
+        //System.out.println("Recherche de 'banboun' : " + myZoo.searchAnimal("banboun"));
+
+        //System.out.println(myZoo);
     }
 }
